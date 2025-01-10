5f0f83d15062b215b2806204657a7db7f34c3d9b
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2025-01-10 11:17:08 +0100

    added header

---
+++ linkedlist.h
@@ -0,0 +1,17 @@
+#ifndef LINKEDLIST_H
+# define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+
+void print_vals(t_node *head);
+
+#endif
